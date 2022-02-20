94eeb872d55bc529e9a0360d21e3d162ac1cff28
Kwangwoo <bnc3049@gmail.com>
2022-02-20 09:45:43 -0700

    CHange

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,17 +1,18 @@
-import { InjectedConnector } from '@web3-react/injected-connector'
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
-const NETWORK_URL = process.env.REACT_APP_NETWORK_URL ? process.env.REACT_APP_NETWORK_URL : "https://bscrpc.com"
-
+const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
+  ? process.env.REACT_APP_NETWORK_URL
+  : "https://bscrpc.com";
 
 export const injected = new InjectedConnector({
   supportedChainIds: [56, 97],
-})
+});
 
 // mainnet only
 export const walletconnect = new WalletConnectConnector({
   rpc: { 56: NETWORK_URL },
-  bridge: 'https://bridge.walletconnect.org',
+  bridge: "https://bridge.walletconnect.org",
   qrcode: true,
-  pollingInterval: 15000,
-})
+  // pollingInterval: 15000,
+});
--- src/utils/web3React.ts
+++ src/utils/web3React.ts
@@ -1,25 +1,25 @@
-import { InjectedConnector } from '@web3-react/injected-connector'
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
-import { ethers } from 'ethers'
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { ethers } from "ethers";
 
 enum ConnectorNames {
   Injected = "injected",
   WalletConnect = "walletconnect",
 }
-const POLLING_INTERVAL = 12000
-const chainId = 56
-const NETWORK_URL = process.env.REACT_APP_NETWORK_URL ? process.env.REACT_APP_NETWORK_URL : "https://bscrpc.com"
-const injected = new InjectedConnector({ supportedChainIds: [chainId] })
+const POLLING_INTERVAL = 12000;
+const chainId = 56;
+const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
+  ? process.env.REACT_APP_NETWORK_URL
+  : "https://bscrpc.com";
+const injected = new InjectedConnector({ supportedChainIds: [chainId] });
 
 const walletconnect = new WalletConnectConnector({
   rpc: { [chainId]: NETWORK_URL },
   qrcode: true,
-  pollingInterval: POLLING_INTERVAL,
-})
-
+  // pollingInterval: POLLING_INTERVAL,
+});
 
 export const connectorsByName = {
   [ConnectorNames.Injected]: injected,
   [ConnectorNames.WalletConnect]: walletconnect,
-}
-
+};
