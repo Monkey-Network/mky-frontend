37414f2cb50ee1ac72d4b735df26416dd4ef7dd8
Kwangwoo <bnc3049@gmail.com>
2022-02-20 20:26:02 -0700

    Fixed word

--- src/components/ui/MonPadTitle.js
+++ src/components/ui/MonPadTitle.js
@@ -1,115 +1,107 @@
-import styled from 'styled-components'
-import Countdown from 'react-countdown';
-import { useCurrentBlockNumber } from '../../hooks';
-const Completionist = () => <span>(NOW LIVE)</span>;
+import styled from "styled-components";
+import Countdown from "react-countdown";
+import { useCurrentBlockNumber } from "../../hooks";
+const Completionist = () => <span></span>;
 
 const MonPadTitle = () => {
-    const blockNumber = useCurrentBlockNumber()
+  const blockNumber = useCurrentBlockNumber();
 
+  return (
+    <Title>
+      <TitleContent>
+        <h2>
+          MONKEY NETWORK STATISTICS &nbsp;
+          {blockNumber != 0 && blockNumber < 1645383600 ? (
+            <>
+              <Countdown date={Date.now() + (1645383600 - blockNumber) * 1000}>
+                <Completionist />
+              </Countdown>
+              &nbsp;&nbsp;LEFT TO LIVE
+            </>
+          ) : (
+            <>
+              <Completionist />
+            </>
+          )}
+        </h2>
+        <h3>Stake $BUSD and earn daily staking rewards</h3>
+        <h3>Stake $MONKEY and earn daily staking rewards</h3>
+      </TitleContent>
+    </Title>
+  );
+};
 
-    return (
-        <Title>
-            <TitleContent>
-                <h2>MONKEY NETWORK STATISTICS &nbsp;
-                    {
-                        blockNumber != 0 && blockNumber < 1645383600 ?
-                        <>
-                            <Countdown date={Date.now() + (1645383600 - blockNumber) * 1000}>
-                                <Completionist />
-                            </Countdown>
-                            &nbsp;&nbsp;LEFT TO LIVE
-                        </>
-                        :
-                        <>
-                            <Completionist />
-                        </>
-                    }
-                </h2>
-                <h3>Stake $BUSD and earn daily staking rewards</h3>
-                <h3>Stake $MONKEY and earn daily staking rewards</h3>
-            </TitleContent>
-
-        </Title>
-    )
-}
-
-export default MonPadTitle
+export default MonPadTitle;
 
 const Title = styled.div`
-    background-color: rgb(217, 125, 62);
-    border-style: solid;
-    border-width: 1.9px;
-    border-color: rgba(255, 255, 255, 0.9);
-    border-bottom-color: rgb(217, 125, 62);
-    border-radius: 30px 30px 0 0;
-    margin-top: -1px;
-    display: flex;
-    align-items: left;
-    text-align: left;
-    width: 100%;
-    margin-bottom: 30px;
+  background-color: rgb(217, 125, 62);
+  border-style: solid;
+  border-width: 1.9px;
+  border-color: rgba(255, 255, 255, 0.9);
+  border-bottom-color: rgb(217, 125, 62);
+  border-radius: 30px 30px 0 0;
+  margin-top: -1px;
+  display: flex;
+  align-items: left;
+  text-align: left;
+  width: 100%;
+  margin-bottom: 30px;
 `;
 
 const TitleImg = styled.div`
-    margin-right: 20px;
-    img{
-        width: 115px;
+  margin-right: 20px;
+  img {
+    width: 115px;
+  }
+  @media screen and (max-width: 960px) {
+    margin-right: 15px;
+    img {
+      width: 80px;
     }
-    @media screen and (max-width: 960px) {
-        
-        margin-right: 15px;
-        img{
-            width: 80px;
-        }
-    }
+  }
 `;
 
 const TitleContent = styled.div`
-    padding-left: 30px;
-    h2{
-        color: rgb(5, 58, 12)!important;
-        font-size: 1.6em;
-        font-weight: 600;
-        letter-spacing: -0.02em;
-        text-transform: none;
-        margin-top: 10px;
-        margin-bottom: 10px;
-    }
-    h3{
-        font-size: 1em;
-        margin-bottom: 0;
-        color: rgb(5, 58, 12);
-        font-weight: 600;
-        letter-spacing: -0.02em;
-        text-transform: none;
-        margin-top: 10px;
-    }
-    a{
-        position: relative;
-        z-index: 12;
-        color: rgb(124, 91, 255);
-        font-size: 1.2em;
-        &:hover{
-            opacity: .7;
-        }
-        
-        display: inline-block;
+  padding-left: 30px;
+  h2 {
+    color: rgb(5, 58, 12) !important;
+    font-size: 1.6em;
+    font-weight: 600;
+    letter-spacing: -0.02em;
+    text-transform: none;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  h3 {
+    font-size: 1em;
+    margin-bottom: 0;
+    color: rgb(5, 58, 12);
+    font-weight: 600;
+    letter-spacing: -0.02em;
+    text-transform: none;
+    margin-top: 10px;
+  }
+  a {
+    position: relative;
+    z-index: 12;
+    color: rgb(124, 91, 255);
+    font-size: 1.2em;
+    &:hover {
+      opacity: 0.7;
     }
 
-    @media screen and (max-width: 960px) {
+    display: inline-block;
+  }
 
-        h2, h3{
-       
-            font-size: 1em;
-            line-height: 26px;
- 
-        }
-        a{
- 
-           
-            font-size: .9em;
-            margin-bottom: 0;
-        }
+  @media screen and (max-width: 960px) {
+    h2,
+    h3 {
+      font-size: 1em;
+      line-height: 26px;
     }
+    a {
+      font-size: 0.9em;
+      margin-bottom: 0;
+    }
+  }
 `;
-
